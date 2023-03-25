9bac209a6158e3ca680732e5a3a27109ebee1feb
Beaads <biacriciuma@hotmail.com>
2023-03-24 23:04:41 -0300

    alterando para lazyColumn e lazyRow e mudando o layout

--- app/src/main/java/com/example/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/aluvery/ui/screens/HomeScreen.kt
@@ -1,43 +1,75 @@
 package com.example.aluvery.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.aluvery.model.Product
+import com.example.aluvery.sampledata.sampleProducts
 import com.example.aluvery.sampledata.sampleSections
+import com.example.aluvery.ui.components.CardProductItem
 import com.example.aluvery.ui.components.ProductSection
+import com.example.aluvery.ui.components.SearchTextField
 import com.example.aluvery.ui.theme.AluveryTheme
 
 @Composable
 fun HomeScreen(
-    sections: Map<String, List<Product>>
+    sections: Map<String, List<Product>>,
+    searchText: String = ""
 ) {
     Column {
-        var text by remember { mutableStateOf("") }
-        OutlinedTextField(value = text, onValueChange = { newValue ->
-        text = newValue
-        })
+        var text by remember {
+            mutableStateOf(searchText)
+        }
+        SearchTextField(
+            searchText = text,
+            onSearchChange = {
+                text = it
+            },
+            Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+        )
+        val searchedProducts = remember(text) {
+            if (text.isNotBlank()) {
+                sampleProducts.filter { product ->
+                    product.name.contains(
+                        text,
+                        ignoreCase = true,
+                    ) ||
+                            product.description?.contains(
+                                text,
+                                ignoreCase = true,
+                            ) ?: false
+                }
+            } else emptyList()
+        }
         LazyColumn(
             Modifier
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(vertical = 16.dp)
+            contentPadding = PaddingValues(bottom = 16.dp)
         ) {
-            for (section in sections) {
-                val title = section.key
-                val products = section.value
-                item {
-                    ProductSection(
-                        title = title,
-                        products = products
+            if (text.isBlank()) {
+                for (section in sections) {
+                    val title = section.key
+                    val products = section.value
+                    item {
+                        ProductSection(
+                            title = title,
+                            products = products
+                        )
+                    }
+                }
+            } else {
+                items(searchedProducts) { p ->
+                    CardProductItem(
+                        product = p,
+                        Modifier.padding(horizontal = 16.dp),
                     )
                 }
             }
@@ -55,3 +87,15 @@ private fun HomeScreenPreview() {
     }
 }
 
+@Preview
+@Composable
+fun HomeScreenWithSearchTextPreview() {
+    AluveryTheme {
+        Surface {
+            HomeScreen(
+                sampleSections,
+                searchText = "a",
+            )
+        }
+    }
+}
