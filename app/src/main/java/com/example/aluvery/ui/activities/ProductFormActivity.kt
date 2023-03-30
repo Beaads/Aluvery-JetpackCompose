3b1a4d8039b90f412d743de0dc0950eb94962c9f
Beaads <biacriciuma@hotmail.com>
2023-03-29 21:18:39 -0300

    continua implementação

--- app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
@@ -1,19 +1,33 @@
 package com.example.aluvery.ui.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.aluvery.R
+import com.example.aluvery.model.Product
 import com.example.aluvery.ui.theme.AluveryTheme
+import java.math.BigDecimal
+import java.text.DecimalFormat
 
 class ProductFormActivity : ComponentActivity() {
 
@@ -35,60 +49,127 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(text = "Criando o produto",
+        Spacer(modifier = Modifier)
+        Text(
+            text = "Criando o produto",
             Modifier.fillMaxWidth(),
-            fontSize = 28.sp)
+            fontSize = 28.sp
+        )
         var url by remember {
             mutableStateOf("")
         }
-        TextField(value = url,
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url, contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
+        }
+        TextField(
+            value = url,
             onValueChange = {
-            url = it
-        }, Modifier.fillMaxWidth(),
+                url = it
+            }, Modifier.fillMaxWidth(),
             label = {
-            Text(text = "Url da imagem")
-        })
+                Text(text = "Url da imagem")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next
+            )
+        )
 
         var name by remember {
             mutableStateOf("")
         }
-        TextField(value = name,
+        TextField(
+            value = name,
             onValueChange = {
-            name = it
-        }, Modifier.fillMaxWidth(),
+                name = it
+            }, Modifier.fillMaxWidth(),
             label = {
-            Text(text = "Nome")
-        })
+                Text(text = "Nome")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+                capitalization = KeyboardCapitalization.Words
+            )
+        )
 
         var price by remember {
             mutableStateOf("")
         }
-        TextField(value = price,
+        val formatter = remember {
+            DecimalFormat("#.##")
+        }
+        TextField(
+            value = price,
             onValueChange = {
-            price = it
-        }, Modifier.fillMaxWidth(),
+                try {
+                    price = formatter.format(BigDecimal(it))
+                } catch (e: java.lang.IllegalArgumentException) {
+                    if(it.isBlank()) {
+                        price = it
+                    }
+                }
+            }, Modifier.fillMaxWidth(),
             label = {
-            Text(text = "Preço")
-        })
-
+                Text(text = "Preço")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next
+            )
+        )
         var description by remember {
             mutableStateOf("")
         }
-        TextField(value = description,
+        TextField(
+            value = description,
             onValueChange = {
-            description = it
-        }, Modifier.fillMaxWidth()
+                description = it
+            },
+            Modifier
+                .fillMaxWidth()
                 .heightIn(min = 100.dp),
             label = {
-            Text(text = "Descrição")
-        })
+                Text(text = "Descrição")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Sentences
+            )
+        )
 
-        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+        Button(
+            onClick = {
+                val convertedPrice = try {
+                    BigDecimal(price)
+                } catch (e: NumberFormatException) {
+                    BigDecimal.ZERO
+                }
+                val product = Product(
+                    name = name,
+                    image = url,
+                    price = convertedPrice,
+                    description = description
+                )
+                Log.i("", "")
+            },
+            Modifier.fillMaxWidth()
+        ) {
             Text(text = "Salvar")
         }
+        Spacer(modifier = Modifier)
     }
 }
 
